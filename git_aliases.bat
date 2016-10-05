697f830422d5d261ff1c386b7db1d36cc69fd95d
Alan Peters <alan3051@gmail.com>
2016-10-05 14:29:23 -0600

    Adding additional log and diff options
    New log options added to limit the number of commits which are returned.
    Adding new diff commands to show differences in staged and working
    directory files. Adding branch --list with -a flag to show remote
    branches as well.

--- git_aliases.bat
+++ git_aliases.bat
@@ -2,13 +2,19 @@ REM global operator
 SET operators="--global"
 
 REM general
-git config --global user.name "Your Name"
-git config --global user.email YourEmail@example.com
+REM git config --global user.name "Your Name"
+REM git config --global user.email YourEmail@example.com
 
 
 REM log aliases
 git config --global alias.lg "log --graph --pretty=oneline --decorate --all --abbrev-commit"
+git config --global alias.lg10 "log -n 10 --graph --pretty=oneline --decorate --all --abbrev-commit"
+git config --global alias.lg20 "log -n 20 --graph --pretty=oneline --decorate --all --abbrev-commit"
+
 git config --global alias.last "log -p -1"
+git config --global alias.d "diff HEAD"
+git config --global alias.ds "diff --cached HEAD"
+
 
 REM commit
 git config --global alias.c commit
@@ -19,7 +25,6 @@ REM Status
 git config --global alias.st status
 
 REM Branches
-git config --global alias.bl "branch --list"
+git config --global alias.bl "branch --list -a"
 git config --global alias.co checkout
 git config --global alias.b "checkout -b "
-
