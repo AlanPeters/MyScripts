0d8b8deb4b10a9aa3062509c894a00d8bdec8f10
Alan Peters <alan3051@gmail.com>
2016-10-28 09:14:34 -0600

    Changing --global to be optional
    Also removed checkout -b as it is not working correctly

--- git_aliases.bat
+++ git_aliases.bat
@@ -1,30 +1,26 @@
 REM global operator
-SET operators="--global"
-
-REM general
-REM git config --global user.name "Your Name"
-REM git config --global user.email YourEmail@example.com
-
+REM Delete --global if you only want to use for one repository
+SET operators=--global
 
 REM log aliases
-git config --global alias.lg "log --graph --pretty=oneline --decorate --all --abbrev-commit"
-git config --global alias.lg10 "log -n 10 --graph --pretty=oneline --decorate --all --abbrev-commit"
-git config --global alias.lg20 "log -n 20 --graph --pretty=oneline --decorate --all --abbrev-commit"
+git config %operators% alias.lg "log --graph --pretty=oneline --decorate --all --abbrev-commit"
+git config %operators% alias.lg10 "log -n 10 --graph --pretty=oneline --decorate --all --abbrev-commit"
+git config %operators% alias.lg20 "log -n 20 --graph --pretty=oneline --decorate --all --abbrev-commit"
 
-git config --global alias.last "log -p -1"
-git config --global alias.d "diff HEAD"
-git config --global alias.ds "diff --cached HEAD"
+REM last commit and diff
+git config %operators% alias.last "log -p -1"
+git config %operators% alias.d "diff HEAD"
+git config %operators% alias.ds "diff --cached HEAD"
 
 
 REM commit
-git config --global alias.c commit
-git config --global alias.cs "commit -m SAVE"
-git config --global alias.cm "commit -m"
+git config %operators% alias.c commit
+git config %operators% alias.cs "commit -m SAVE"
+git config %operators% alias.cm "commit -m"
 
 REM Status
-git config --global alias.st status
+git config %operators% alias.st status
 
 REM Branches
-git config --global alias.bl "branch --list -a"
-git config --global alias.co checkout
-git config --global alias.b "checkout -b "
+git config %operators% alias.bl "branch --list -a"
+git config %operators% alias.co checkout
